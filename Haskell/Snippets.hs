a51b8200f00ace7f4f9829003113622d79bc568a
Timothy Hwang <Tim.Hwang@sizmek.com>
2018-06-06 13:03:32 -0400

    Add snnippet: makeBinary

--- Haskell/Snippets.hs
+++ Haskell/Snippets.hs
@@ -68,3 +68,11 @@ houserobber :: [Int] -> Int
 houserobber = snd . (foldr f (0, 0)) . reverse
   where
     f n (prevMax, currMax) = (currMax, max currMax (prevMax + n))
+
+makeBinary :: Int -> [Int]
+makeBinary 0 = []
+makeBinary 1 = [0, 1]
+makeBinary n = prevList ++ newList
+  where
+    prevList = makeBinary (n - 1)
+    newList = map (\x -> x + 10 ^ (n - 1)) prevList
